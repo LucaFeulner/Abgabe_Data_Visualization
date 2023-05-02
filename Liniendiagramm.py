dc9690bae057c4c83a11602c53ad8e4ffe1acaf5
LucaFeulner <Luca_feulner@outlook.de>
2023-05-02 23:16:24 +0200

    jetzt geht alles davor falschen liniengramm

--- Liniendiagramm.py
+++ Liniendiagramm.py
@@ -15,32 +15,29 @@ import pandas as pd
 # Fertiges Datenset was in Datensets.py bearbeitet wurde einlesen
 df = pd.read_csv("Abgabe/res/FertigesDatenset.csv")
 
-zehn_groeßte_airline = df.groupby("AIRLINE")["FLIGHT_NUMBER"].count()
-zehn_groeßte_airline = zehn_groeßte_airline.reset_index()
-zehn_groeßte_airline = zehn_groeßte_airline.rename(columns={"FLIGHT_NUMBER" : "count"})
-zehn_groeßte_airline = zehn_groeßte_airline.sort_values(by ="count", ascending= False)
-zehn_groeßte_airline  = zehn_groeßte_airline.reset_index()
-zehn_groeßte_airline = zehn_groeßte_airline.loc[zehn_groeßte_airline['count'] >= 13151]
-zehn_groeßte_airline = zehn_groeßte_airline[["AIRLINE", "count"]]
+fluege_pro_airline = df.groupby("AIRLINE")["FLIGHT_NUMBER"].count()
+zehn_groeßten_airlines = fluege_pro_airline.nlargest(10).index
 
+datenset_zehn = df[df["AIRLINE"].isin(zehn_groeßten_airlines)]
 
-
+farben = ["#143d59", "#e71989", "#b8df10", "#f6b60d", "#5e057e", "#a2eacb", "#761137", "#514644", "#0f4d19", "#104c91"]
+farben_zaehler = 0
 fig = go.Figure()
 
 # Datenset das nach Airline und dann nach Tagen gruppiert wurde, und die Anzahl der Flugnummer zählt, da Flugnummern einmalig sind
-fluege_anzahl = df.groupby(["AIRLINE", "DAY"])["FLIGHT_NUMBER"].count().reset_index()
+fluege_anzahl = datenset_zehn.groupby(["AIRLINE", "DAY"])["FLIGHT_NUMBER"].count().reset_index()
 
 for airline in fluege_anzahl["AIRLINE"].unique():
     fig.add_trace(go.Scatter(
                             x = fluege_anzahl[fluege_anzahl["AIRLINE"] == airline ]["DAY"],
-                            y =fluege_anzahl[fluege_anzahl["AIRLINE"] == airline]["FLIGHT_NUMBER"]
+                            y =fluege_anzahl[fluege_anzahl["AIRLINE"] == airline]["FLIGHT_NUMBER"],
+                            name = airline,
+                            line_color = farben[farben_zaehler]
+
                             
-    )
-
-
-
-
-    )
+                            )
+                )
+    farben_zaehler = farben_zaehler +  1
 
 fig.update_layout(  title="Anzahl der Flüge pro Tag und Fluggesellschaft im Januar 2015",
                     xaxis = dict(

f79e4748bf6694a1d1df6ceb97d93cb56863baf5
LucaFeulner <Luca_feulner@outlook.de>
2023-05-03 12:43:06 +0200

    funkt

--- Liniendiagramm.py
+++ Liniendiagramm.py
@@ -23,13 +23,16 @@ datenset_zehn = df[df["AIRLINE"].isin(zehn_groeßten_airlines)]
 farben = ["#143d59", "#e71989", "#b8df10", "#f6b60d", "#5e057e", "#a2eacb", "#761137", "#514644", "#0f4d19", "#104c91"]
 farben_zaehler = 0
 fig = go.Figure()
+datenset_zehn['DAY'] = datenset_zehn['DAY'].map(str).apply(lambda x: x.zfill(2))
 
+
+datenset_zehn["DATUM"]=datenset_zehn["DAY"].map(str) + "-" + datenset_zehn["MONTH"].map(str)+"-" + datenset_zehn["YEAR"].map(str)
 # Datenset das nach Airline und dann nach Tagen gruppiert wurde, und die Anzahl der Flugnummer zählt, da Flugnummern einmalig sind
-fluege_anzahl = datenset_zehn.groupby(["AIRLINE", "DAY"])["FLIGHT_NUMBER"].count().reset_index()
+fluege_anzahl = datenset_zehn.sort_values(by="DAY").groupby(["AIRLINE", "DATUM"])["FLIGHT_NUMBER"].count().reset_index()
 
 for airline in fluege_anzahl["AIRLINE"].unique():
     fig.add_trace(go.Scatter(
-                            x = fluege_anzahl[fluege_anzahl["AIRLINE"] == airline ]["DAY"],
+                            x = fluege_anzahl[fluege_anzahl["AIRLINE"] == airline ]["DATUM"],
                             y =fluege_anzahl[fluege_anzahl["AIRLINE"] == airline]["FLIGHT_NUMBER"],
                             name = airline,
                             line_color = farben[farben_zaehler]
@@ -41,7 +44,7 @@ for airline in fluege_anzahl["AIRLINE"].unique():
 
 fig.update_layout(  title="Anzahl der Flüge pro Tag und Fluggesellschaft im Januar 2015",
                     xaxis = dict(
-                                tickvals = fluege_anzahl['DAY'].unique(),
+                                tickvals = fluege_anzahl['DATUM'].unique(),
                                 gridcolor = "lightgray",
                                 title = "Tag",
                                 ),
--- mapbox.py
+++ mapbox.py
@@ -1,2 +1,29 @@
-# Flughäfen auf einer Map anzeigen
+import plotly.graph_objects as go
+
+import Layout
+import pandas as pd
+#_______________________________________________________________________________________________________________________________________________________
+
+#________________________________________________________Mapbox____________________________________________________________________________________
+#_______________________________________________________________________________________________________________________________________________________
+
+
+
+
+# Abflughäfen auf einer Map anzeigen
 # Größe der Punkte ist die Anzahl wie oft dort der Starts (pro Tag)
+# Für eine bessere übersicht nur die größten 20 Flughafen
+
+df = pd.read_csv("Abgabe/res/FertigesDatenset.csv")
+
+# Datensatznach Abflughafren gruppiert und die Flugnummern gezählt
+fluege_pro_flughafen = df.groupby("ORIGIN_AIRPORT")["FLIGHT_NUMBER"].count()
+
+# Die 20 größten Flughafenkürzel in der variablen speichern
+zwanzig_groeßten_Flughafen= fluege_pro_flughafen.nlargest(20).index
+
+
+
+# neues kleines Datenset anlegen --> es werden nur die Zeilen aus dem großen Datenset genommen, wo  die kürzel mit der oben erstellten variable übereinstimmen
+datenset_zwanzig = df[df["ORIGIN_AIRPORT"].isin(zwanzig_groeßten_Flughafen)]
+
